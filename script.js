e0f25e8af0807169fb1f7374a4ea8e4d66eb8adf
lesliehealray <lesliehealray@gmail.com>
2019-09-19 19:22:16 -0400

    initial commit form validation

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Add validation!');
+document.getElementById('parking-form').addEventListener('submit',function(evt){
+    evt.preventDefault();   
+
+    let formInputs = document.getElementsByTagName('input');
+    let i;
+    for (i = 0; i < htmlTags.length; i++) {
+        if (formInputs[i].value === ""){
+        formInputs[i].insertAdjacentHTML('afterend', '<span class="error">Required Field</span>')
+        }
+    }
+})
+
+
+// added event listener to the form which listens for a submit at which time a function occurs
+// let i holds the values as you loop through 
+//formInput is a collection of input elements and we check the value of each item in the collection in the for loop
+//we have to iterate through the collection and access them by their index. 
+//https://www.w3schools.com/jsref/met_node_insertadjacenthtml.asp
+
+
